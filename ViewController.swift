0e0100506051e21203a8e23d2f079c502dcf7a79
VENKATA SAI RAM DASARI <98133671+VENKATSAIDASARI@users.noreply.github.com>
2022-09-13 10:12:37 -0500

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  DvAssign
+//
+//  Created by Pati,Usha R on 9/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    
+    @IBOutlet weak var fullNameLabel: UILabel!
+    
+    
+    @IBOutlet weak var initialsLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func onClickOfSubmit(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func onClickOfReset(_ sender: UIButton) {
+    }
+    
+
+}
+
